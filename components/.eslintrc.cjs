e8ecd2860742063c148abf7cbdee3a9b8508f27a
Jared L <48422312+lhjt@users.noreply.github.com>
2022-11-21 13:42:51 +1100

    chore(components): add `eslint` config

---
+++ components/.eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  settings: {
+    react: {version: 'detect'}
+  },
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'always'
+    ]
+  }
+};
+
--- components/vite.config.ts
+++ components/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
+import ESLint from "vite-plugin-linter";
+
+import packageJson from "./package.json";
+
+const { EsLinter, linterPlugin } = ESLint;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default defineConfig((configEnv) => ({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    linterPlugin({
+      include: ["./src/**/*.{ts,tsx}"],
+      linters: [new EsLinter({ configEnv })],
+    }),
+    dts({
+      include: ["src/components/"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve("src", "index.ts"),
+      name: "@csesoc/ui-components",
+      formats: ["es"],
+      fileName: (format) => `csesoc-ui-components.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
+    },
+  },
+}));
