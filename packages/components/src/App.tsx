49c2e8de4f8e974212879a64274a9582efa8480a
Merry Rosalie <z5391208@ad.unsw.edu.au>
2024-05-17 14:40:07 +0700

    chore: update deprecated dependencies  (#29)
    * chore: update all dependencies
    * chore: migrated storybook to storybook 8.1.1
    * chore: fixed export issues in vite.config.ts
    * chore: added latest node version to npmrc

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className='App'>
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
+        <a href='https://vitejs.dev' target='_blank' rel='noopener noreferrer'>
           <img src='/vite.svg' className='logo' alt='Vite logo' />
         </a>
-        <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
+        <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
