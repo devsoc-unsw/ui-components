170de084538e04199bcbe9b6224983660fc8a49b
Merry Rosalie <z5391208@ad.unsw.edu.au>
2024-07-08 21:00:38 +1000

    style: clean up App.tsx

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -2,22 +2,11 @@ import './App.css';
 
 import { useState } from 'react';
 
-import reactLogo from './assets/react.svg';
-import { Dropdown } from './dropdown';
-
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className='App'>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noopener noreferrer'>
-          <img src='/vite.svg' className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
