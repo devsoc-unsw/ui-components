161ff4a3bfe8a89069efb5b9bbe69a6cebc81870
Merry Rosalie <z5391208@ad.unsw.edu.au>
2024-07-08 20:56:35 +1000

    style: removed dropdown component from App.tsx

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -25,7 +25,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <Dropdown options={[{ value: 'smth', label: 'smth' }]} />
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </div>
   );
